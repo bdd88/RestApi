37e634c4b26b22534ca260ca0c92da03629fe8ee
Bradley Duke <bradduke88@gmail.com>
2022-08-10 14:41:07 -0500

    Added ServiceContainer dependency.

--- src/Controller/Main.php
+++ src/Controller/Main.php
@@ -1,29 +1,29 @@
 <?php
 namespace Bdd88\RestApi\Controller;
 
-use Bdd88\RestApi\Model\ConfigDatabase;
+use Bdd88\ServiceContainer\ServiceContainer;
 use Bdd88\RestApi\Model\EndpointAbstract;
-use Bdd88\RestApi\Model\Request;
-use Bdd88\MySql\MySql;
 use Throwable;
 
 /** Primary controller that handles flow of data between the user, sub-controllers, and models. */
 class Main
 {
-    private MySql $database;
-    private Router $router;
-    private Request $request;
-    private ConfigDatabase $databaseConfig;
+    private ServiceContainer $serviceContainer;
     private bool $debugMode;
 
-    public function __construct(string $databaseConfigPath, ?string $debugMode = NULL)
+    /**
+     * @param string $databaseConfigPath Path to ini file containing database credentials.
+     * @param string $publicKey PEM formatted Public Key for RSA, and Shared Secret for HMAC.
+     * @param string|null $privateKey (optional) PEM formatted RSA private key to use for signing tokens.
+     * @param string|null $debugMode (optional) TRUE will display errors/exceptions as they occur to the end user. FALSE will hide errors behind a generic message.
+     */
+    public function __construct(string $databaseConfigPath, string $publicKey, ?string $privateKey = NULL, ?string $debugMode = NULL)
     {
         $this->debugMode = $debugMode ?? FALSE;
         set_exception_handler(array($this, 'exceptionHandler'));
-        $this->databaseConfig = new ConfigDatabase($databaseConfigPath);
-        $this->database = new MySql(...$this->databaseConfig->getAllSettings());
-        $this->request = new Request();
-        $this->router = new Router();
+        $this->serviceContainer = new ServiceContainer();
+        $this->serviceContainer->create('\Bdd88\RestApi\Model\ConfigDatabase', [$databaseConfigPath]);
+        $this->serviceContainer->create('\Bdd88\RestApi\Controller\Router');
     }
 
     public function exceptionHandler(Throwable $exception): void
@@ -46,9 +46,10 @@ class Main
     /** Process the requested URI, and serve the appropriate endpoint. */
     public function exec()
     {
-        $destination = $this->router->route($this->request->endpointName);
+        /** @var Router $router */
+        $router = $this->serviceContainer->get('\Bdd88\RestApi\Controller\Router');
         /** @var EndpointAbstract $endpoint */
-        $endpoint = new $destination($this->database, $this->request);
+        $endpoint = $this->serviceContainer->create($router->route());
         echo $endpoint->serve();
     }
 
