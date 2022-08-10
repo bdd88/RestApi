7f7e28efaac10f435849b44d5f576cf69f641383
Bradley Duke <bradduke88@gmail.com>
2022-08-10 14:41:48 -0500

    Router class now uses Request and Login classes.

--- src/Controller/Router.php
+++ src/Controller/Router.php
@@ -1,12 +1,22 @@
 <?php
 namespace Bdd88\RestApi\Controller;
 
+use Bdd88\RestApi\Model\Request;
 use Exception;
 
 /** Determines what classes to load based on a given request, and processes URI data for usage. */
 class Router
 {
+    private Request $request;
+    private Login $login;
     private array $endpoints;
+
+    public function __construct(Request $request, Login $login)
+    {
+        $this->request  = $request;
+        $this->login = $login;
+    }
+
 
     /** Create a mapping for endpoint name to class. */
     public function createEndpoint(string $endpointName, string $endpointClass): void
@@ -15,16 +25,16 @@ class Router
     }
 
     /**
-     * Determine the correct way to route a request based on the endpoint name.
+     * Determine the correct way to route a request based on the endpoint name supplied by the request.
      *
      * @return string The class name or http status code for the routed request.
      */
-    public function route(string $endpointName): string
+    public function route(): string
     {
-        if (!isset($this->endpoints[$endpointName])) {
-            throw new Exception('Endpoint doesn\'t exist: \'' . $endpointName . '\'');
+        if (!isset($this->endpoints[$this->request->endpointName])) {
+            throw new Exception('Endpoint doesn\'t exist: \'' . $this->request->endpointName . '\'');
         } else {
-            return $this->endpoints[$endpointName];
+            return $this->endpoints[$this->request->endpointName];
         }
     }
 
