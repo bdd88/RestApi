2e440242b96f928bf0266ce9c40b02375d24abb6
Bradley Duke <bradduke88@gmail.com>
2022-08-15 16:56:14 -0500

    Better readability & error handling for requests.

--- src/Model/Request.php
+++ src/Model/Request.php
@@ -6,44 +6,58 @@ use Exception;
 /** Stores and parses data provided by the user request. */
 class Request
 {
-    public string $uri;
     public string $method;
-    public string|null $payload;
-    public string $endpointName;
+    public string|FALSE|NULL $tokenString;
+    public string|FALSE $endpointName;
     public array $endpointValues;
-
-    public string $tokenString;
+    public string|NULL $payload;
 
     public function __construct()
     {
-        $this->uri = $_SERVER['REQUEST_URI'];
         $this->method = $_SERVER['REQUEST_METHOD'];
-        $this->authorization = $_SERVER['HTTP_AUTHORIZATION'];
+        $this->accept = $_SERVER['HTTP_ACCEPT'];
+        $this->tokenString = $this->parseTokenString($_SERVER['HTTP_AUTHORIZATION']);
+        $this->endpointName = $this->parseUri($_SERVER['REQUEST_URI']);
         $this->payload = file_get_contents("php://input");
-        $this->parse();
     }
 
-    /** Parse a URI request string. */
-    private function parse(): void
+    /**
+     * Parse the authorization header and return the token string.
+     *
+     * @param string $authorization Authorization header from the client.
+     * @return string|FALSE|NULL The token string from the auth header. FALSE if it's not a BEARER token. NULL if no authorization header was provided.
+     */
+    private function parseTokenString(string $authorization): string|FALSE|NULL
     {
-        // Retrieve the bearer token for authorization.
-        list($authScheme, $authParameter) = explode(' ', $this->authorization);
-        if ($authScheme !== 'Bearer') {
-            throw new Exception('Wrong HTTP authorization scheme (or improperly formatted)');
+        if ($authorization === NULL) {
+            return NULL;
         }
-        $this->tokenString = $authParameter;
+        list($authScheme, $authParameter) = explode(' ', $authorization);
+        if ($authScheme !== 'Bearer') {
+            return FALSE;
+        }
+        return $authParameter;
+    }
 
+    /**
+     * Parse a URI request string.
+     *
+     * @param string $uri Requested URI.
+     * @return string|FALSE The endpoint name requested. FALSE if the request is malformed.
+     */
+    private function parseUri(string $uri): string|FALSE
+    {
         // Parse the URI and remove the script name from the request path.
-        $requestArray = parse_url($this->uri);
+        $requestArray = parse_url($uri);
         $requestPath = array_values(array_diff(
             explode('/', $requestArray['path']),
             explode('/', $_SERVER['SCRIPT_NAME'])
         ));
 
-        // Validate the request path.
+        // Set the endpoing name to FALSE and return early if the URI request is malformed.
         $requestEntries = sizeof($requestPath);
         if ($requestEntries % 2 !== 0) {
-            throw new Exception('Improperly formatted URI: Missing endpoint name or value.');
+            return FALSE;
         }
 
         // Create a map of endpoint name/value pairs.
@@ -55,10 +69,9 @@ class Request
             }
         }
 
-        // Set the endpoint name.
-        $this->endpointName = implode('/', array_keys($this->endpointValues));
+        // Return the endpoint name without values.
+        return implode('/', array_keys($this->endpointValues));
     }
-
 
 }
 
